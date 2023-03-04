8e57cdca35d9e89dce5960a1b5d70fba2ee35ce8
Shinya Kudoh <shinya_kd@yahoo.com>
2023-03-04 11:30:47 -0500

    commit tree 20963d893e6725040ebbad966e3fcd83c3eaa05e

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+const employee = {
+    name: "Shinya",
+    streetAddress: "New York",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+  return {
+    ...employee,
+    [key]: value
+};
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+employee[key] = value
+return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+const objEmployee = {...employee}
+delete objEmployee[key]
+return objEmployee
+
+
+
+    // return delete {
+    // ...employee
+    //  }[key]
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
